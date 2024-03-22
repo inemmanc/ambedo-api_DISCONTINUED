324ff6a794d73e1205a4727794750a732f6e5aee
Felipe <nemmanc@protonmail.com>
2024-03-22 18:35:17 -0300

    add: userRoutes-slice

---
+++ src/router/routes/users.go
@@ -0,0 +1,36 @@
+package routes
+
+import "net/http"
+
+var usersRoutes = []DefaultRoute{
+	{
+		URI:          "/users",
+		Method:       http.MethodGet,
+		Function:     func(w http.ResponseWriter, r *http.Request) {},
+		AuthRequired: false,
+	},
+	{
+		URI:          "/user/{userID}",
+		Method:       http.MethodGet,
+		Function:     func(w http.ResponseWriter, r *http.Request) {},
+		AuthRequired: false,
+	},
+	{
+		URI:          "/user/{userID}",
+		Method:       http.MethodPost,
+		Function:     func(w http.ResponseWriter, r *http.Request) {},
+		AuthRequired: false,
+	},
+	{
+		URI:          "/user/{userID}",
+		Method:       http.MethodPut,
+		Function:     func(w http.ResponseWriter, r *http.Request) {},
+		AuthRequired: false,
+	},
+	{
+		URI:          "/user/{userID}",
+		Method:       http.MethodDelete,
+		Function:     func(w http.ResponseWriter, r *http.Request) {},
+		AuthRequired: false,
+	}
+}
