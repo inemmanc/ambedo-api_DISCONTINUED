2c07ca0ce06045be72b41c4a0d029d6e07cf4c79
Felipe <nemmanc@protonmail.com>
2024-03-26 16:55:56 -0300

    add: Error_responses_func

--- src/responses/responses.go
+++ src/responses/responses.go
@@ -14,3 +14,11 @@ func JSON(w http.ResponseWriter, statusCode int, data interface{}) {
 		log.Fatal(err)
 	}
 }
+
+func Error(w http.ResponseWriter, statusCode int, err error) {
+	JSON(w, statusCode, struct {
+		Error string `json:"error"`
+	}{
+		Error: err.Error(),
+	})
+}
