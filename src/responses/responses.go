1d13415fe19c509efcb30f569ddbad952c1bc7f8
Felipe <nemmanc@protonmail.com>
2024-04-04 23:47:54 -0300

    fix: request_not_allowed_statuscode_does_not_allow_body

--- src/responses/responses.go
+++ src/responses/responses.go
@@ -11,8 +11,10 @@ func JSON(w http.ResponseWriter, statusCode int, data interface{}) {
 	w.Header().Set("Content-type", "application/json")
 	w.WriteHeader(statusCode)
 
-	if err := json.NewEncoder(w).Encode(data); err != nil {
-		log.Fatal(err)
+	if data != nil {
+		if err := json.NewEncoder(w).Encode(data); err != nil {
+			log.Fatal(err)
+		}
 	}
 }
 
