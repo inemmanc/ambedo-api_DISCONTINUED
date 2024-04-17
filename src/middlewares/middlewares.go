0480cc286ead2ed8572b33d0c6f790ffa12305d7
Felipe <nemmanc@protonmail.com>
2024-04-17 18:13:08 -0300

    add: middlewares_Authenticate_func

--- src/middlewares/middlewares.go
+++ src/middlewares/middlewares.go
@@ -1,2 +1,15 @@
 package middlewares
 
+import (
+	"fmt"
+	"net/http"
+)
+
+// Authenticate checks whether the requesting user is authenticated
+func Authenticate(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// TEMP RESPONSE ---- REMOVE
+		fmt.Println("VALIDATING ...")
+		next(w, r)
+	}
+}
