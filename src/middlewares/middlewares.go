852261ae1a01cf9342244c6cd35fc0a7fd23d4f0
Felipe <nemmanc@protonmail.com>
2024-04-17 21:53:43 -0300

    add: Logger_func

--- src/middlewares/middlewares.go
+++ src/middlewares/middlewares.go
@@ -5,6 +5,13 @@ import (
 	"net/http"
 )
 
+func Logger(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Printf("\nLOG: %s %s %s", r.Method, r.RequestURI, r.Host)
+		next(w, r)
+	}
+}
+
 // Authenticate checks whether the requesting user is authenticated
 func Authenticate(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
